efa7dcab7b5b123845e85dadd053a1ec48a15d26
Felipe Rodrigues Luiz <felipe.rodrigues@bry.com.br>
2020-08-12 23:45:01 -0300

    Commit inicial

---
+++ src/main/java/br/com/qualirede/autorizacoes/AutorizacoesProcedimentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.qualirede.autorizacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorizacoesProcedimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorizacoesProcedimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/qualirede/autorizacoes/AutorizacoesProcedimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.qualirede.autorizacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizacoesProcedimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
